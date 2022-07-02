c552d06d9fdf8fb4c30dc6c708bead6121d05951
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-02 17:00:19 -0300

    Feat: added hero ad HTML structure

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,24 @@
+import P from 'prop-types';
+import { Heading } from '../Heading';
 import './styles.scss';
 
 export function Hero() {
    return (
-      <>
-         <h1>Hero</h1>
-      </>
+      <div className="hero">
+         <div className="left-h">
+            <Heading />
+            {/* hero ad */}
+            <div className="the-best-ad">
+               <div></div>
+               <span>the best fitness club in the town</span>
+            </div>
+            {/* hero heading */}
+         </div>
+         <div className="right-h">Right Side</div>
+      </div>
    );
 }
+
+Hero.propTypes = {
+   children: P.node.isRequired,
+};
