22ac908129c0f09967c260710f3c2b8db82781b7
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-02 17:01:19 -0300

    feat: added hero heading HTML structure

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -13,6 +13,21 @@ export function Hero() {
                <span>the best fitness club in the town</span>
             </div>
             {/* hero heading */}
+            <div className="hero-text">
+               <div>
+                  <span className="stroke-text">Shape </span>
+                  <span> Your</span>
+               </div>
+               <div>
+                  <span>Ideal Body</span>
+               </div>
+               <div>
+                  <span>
+                     in here We will help you to shape and build your ideal body
+                     and live up your life to fullest
+                  </span>
+               </div>
+            </div>
          </div>
          <div className="right-h">Right Side</div>
       </div>
