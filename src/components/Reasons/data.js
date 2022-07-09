54247c482c9af27f287fc042b2b49c5e9c083eb4
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-08 21:52:09 -0300

    Feat: Added data of component into separated file

---
+++ src/components/Reasons/data.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
+import image4 from '../../assets/image4.png';
+import nb from '../../assets/nb.png';
+import adidas from '../../assets/adidas.png';
+import nike from '../../assets/nike.png';
+import tick from '../../assets/tick.png';
+
+export const dataImage1 = [
+   { src: image1 },
+   { src: image2 },
+   { src: image3 },
+   { src: image4 },
+];
+
+export const dataImage2 = [
+   { src: tick, text: 'OVER 140+ EXPERT COACHS' },
+   { src: tick, text: 'TRAIN SMARTER AND  FASTER THAN BEFORE' },
+   { src: tick, text: '1 FREE PROGRAM FOR  NEW MEMBER' },
+   { src: tick, text: 'RELIABLE PARTNERS' },
+];
+
+export const partners = [{ src: nb }, { src: adidas }, { src: nike }];
